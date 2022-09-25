633fd9839badf5d04878a7821cb7836139a3c0ef
Hosam-Aldeeb <hosam.1998@hotmail.com>
2022-09-24 23:06:07 -0400

    Tweet CRUD APP Commit

---
+++ public/js/main.js
@@ -0,0 +1,126 @@
+const deleteBtn = document.querySelectorAll('.fa-trash')
+const item = document.querySelectorAll('.item span')
+const itemCompleted = document.querySelectorAll('.item span.completed')
+
+Array.from(deleteBtn).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
+})
+
+Array.from(item).forEach((element)=>{
+    element.addEventListener('click', markComplete)
+})
+
+Array.from(itemCompleted).forEach((element)=>{
+    element.addEventListener('click', markUnComplete)
+})
+
+async function deleteItem(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markUnComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markUnComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+const wrapper = document.querySelector(".wrapper"),
+editableInput = wrapper.querySelector("input"),
+readonlyInput = wrapper.querySelector(".readonly"),
+placeholder = wrapper.querySelector(".placeholder"),
+counter = wrapper.querySelector(".counter"),
+button = wrapper.querySelector("button");
+
+input.onfocus = ()=>{
+  placeholder.style.color = "#c5ccd3";
+}
+input.onblur = ()=>{
+  placeholder.style.color = "#98a5b1";
+}
+
+input.onkeyup = (e)=>{
+  let element = e.target;
+  validated(element);
+}
+input.onkeypress = (e)=>{
+  let element = e.target;
+  validated(element);
+  placeholder.style.display = "none";
+}
+
+function validated(element){
+  let text;
+  let maxLength = 100;
+  let currentlength = element.innerText.length;
+
+  if(currentlength <= 0){
+    placeholder.style.display = "block";
+    counter.style.display = "none";
+    button.classList.remove("active");
+  }else{
+    placeholder.style.display = "none";
+    counter.style.display = "block";
+    button.classList.add("active");
+  }
+
+  counter.innerText = maxLength - currentlength;
+
+  if(currentlength > maxLength){
+    let overText = element.innerText.substr(maxLength); //extracting over texts
+    overText = `<span class="highlight">${overText}</span>`; //creating new span and passing over texts
+    text = element.innerText.substr(0, maxLength) + overText; //passing overText value in textTag variable
+    input.style.zIndex = "1";
+    counter.style.color = "#e0245e";
+    button.classList.remove("active");
+  }else{
+    input.style.zIndex = "-1";
+    counter.style.color = "#333";
+  }
+  input.innerHTML = text; //replacing innerHTML of readonly div with textTag value
+}
